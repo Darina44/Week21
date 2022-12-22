8fcdc42f594965a494eaba9f97c11855eb9bbb57
darina bebenina <darinabebenina@yahoo.com>
2022-12-22 18:13:35 +0300

    lessons

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-    fetch("https://api.github.com/users/Darina44"
-    {
-            headers: {
-                "Content-Type": 'application/json;'
-            }
-        }
+function onSearch() {
+    let gitUser = document.getElementById("userSearch").value;
+
+    fetch("https://api.github.com/users/" + gitUser
     )
         .then(response => response.json())
         .then(user => {
             document.getElementById("avatar").src = user.avatar_url;
             document.getElementById("userName").innerText = user.name;
         })
-        .catch(error) => console.log(error);
-});
+        .catch(error => console.log(error));
+};
